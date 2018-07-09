26b48d0d42f228f282009ffd7785fe7ae463fd43
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-07-09 10:14:11 +0200

    Abot page first stage

---
+++ wp-content/themes/virtualstock/page-about.php
@@ -0,0 +1,114 @@
+<?php
+
+get_header();
+
+?>
+
+<div class="container">
+    <div class="row section-purpose">
+        <div class="section-purpose-content col-md-6">
+            <div class="section-purpose-content-inside">
+                <h2> <?php _e('Our purpose', 'virtual') ?></h2>
+                <p> <?php _e('We provide the platform to connect, the power to control, the edge to compete and a better experience for your colleagues and customers.', 'virtual') ?></p>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <img src="/wp-content/uploads/2018/07/section_purpose.png" />
+        </div>
+    </div>
+
+    <div class="row section-people v-full-width">
+            
+        <div class="container">
+            <h2 class="text-align-center"> <?php _e('Our people', 'virtual') ?></h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="section-people-content-holder upper first">
+                        <img src="/wp-content/uploads/2018/07/andrew_mills.png" />
+                        <h3 class="text-align-center"> <?php _e('Andrew Mills', 'virtual'); ?> </h3>
+                        <h4 class="text-align-center"> <?php _e('CEO', 'virtual'); ?> </h4> 
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="section-people-content-holder upper">
+                        <img src="/wp-content/uploads/2018/07/tim_ingham.png" />
+                        <h3 class="text-align-center"> <?php _e('Tim Ingham', 'virtual'); ?> </h3>
+                        <h4 class="text-align-center"> <?php _e('Co-Founder', 'virtual'); ?> </h4> 
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="section-people-content-holder upper last">
+                        <img src="/wp-content/uploads/2018/07/ed_bradly.png" />
+                        <h3 class="text-align-center"> <?php _e('Ed Bradley', 'virtual'); ?> </h3>
+                        <h4 class="text-align-center"> <?php _e('Co-Founder', 'virtual'); ?> </h4> 
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+            <div class="col-md-3">
+                    <div class="section-people-content-holder lower">
+                        <img src="/wp-content/uploads/2018/07/carolyne_turnbull.png" />
+                        <h3 class="text-align-center"> <?php _e('Carolyne Turnbull', 'virtual'); ?> </h3>
+                        <h4 class="text-align-center"> <?php _e('CEO', 'virtual'); ?> </h4> 
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="section-people-content-holder lower">
+                        <img src="/wp-content/uploads/2018/07/rob_knott.png" />
+                        <h3 class="text-align-center"> <?php _e('Rob Knott', 'virtual'); ?> </h3>
+                        <h4 class="text-align-center"> <?php _e('Co-Founder', 'virtual'); ?> </h4> 
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="section-people-content-holder lower">
+                        <img src="/wp-content/uploads/2018/07/james_thirkill.png" />
+                        <h3 class="text-align-center"> <?php _e('James Thirkill', 'virtual'); ?> </h3>
+                        <h4 class="text-align-center"> <?php _e('Co-Founder', 'virtual'); ?> </h4> 
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="section-people-content-holder lower">
+                        <img src="/wp-content/uploads/2018/07/dale_parkinson.png" />
+                        <h3 class="text-align-center"> <?php _e('Dale Parkinson', 'virtual'); ?> </h3>
+                        <h4 class="text-align-center"> <?php _e('Co-Founder', 'virtual'); ?> </h4> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row section-partners v-full-width">
+        <div class="section-partners-content col-md-6">
+            <div class="section-partners-content-inside">
+                <h2> <?php _e('Our Partners', 'virtual') ?></h2>
+                <p> <?php _e('We’ve developed partnerships with world-renowned businesses and industry leaders who, like us, are dedicated to addressing today’s challenges in new and different ways.', 'virtual') ?></p>
+            </div>
+        </div>
+        <div class="col-md-6 image-holder">
+            <img src="/wp-content/uploads/2018/07/section_purpose.png" />
+        </div>
+    </div>
+
+    <div class="row section-investors v-full-width">
+        <div class="col-md-6 image-holder">
+            <img src="/wp-content/uploads/2018/07/section_purpose.png" />
+        </div>
+        <div class="section-investors-content col-md-6">
+            <div class="section-investors-content-inside">
+                <h2> <?php _e('Our Investors', 'virtual') ?></h2>
+                <p> <?php _e('Stephen Chandler', 'virtual') ?> </p>
+                <p> <?php _e('Managing Partner at Notion Capital:', 'virtual') ?> </p>
+                <p> <?php _e('“Digital supply chain is a hot topic, and for good reason. Virtualstock delivers an agile supply chain technology platform that allows its clients to quickly adapt to the challenges of digitisation without turning to traditional systems integration, thereby reducing cost and risk while accelerating benefits. Virtualstock has already demonstrated this in two important sectors and has accumulated an impressive portfolio of clients. They are now superbly placed to scale rapidly, and we believe that they will become another UK technology success story.”', 'virtual') ?></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php
+
+get_footer();
