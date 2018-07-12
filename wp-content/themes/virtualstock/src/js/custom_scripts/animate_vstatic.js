ebddae045753d97013a34b00895e0d474abbe9d7
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-07-13 01:30:44 +0200

    Configure page to show its own slider

--- wp-content/themes/virtualstock/src/js/custom_scripts/animate_vstatic.js
+++ wp-content/themes/virtualstock/src/js/custom_scripts/animate_vstatic.js
@@ -1,10 +1,17 @@
 
 
 export default (function($) {
+
+
     let smart_slider_is_on_page = document.querySelector('.n2-ss-slider');
-    
+    console.log('blh');
     if( smart_slider_is_on_page != null ) {
-        n2ss.ready(/* slider id */ 2, function(slider, sliderElement) {
+
+        let slider_id = $('.n2-ss-slider').attr('id');
+
+        let smart_slider_id = slider_id.substring(slider_id.length - 1, slider_id.length);
+
+        n2ss.ready(/* slider id */ smart_slider_id, function(slider, sliderElement) {
             jQuery(document).on('switch', function() {
                 jQuery('.v-static').addClass('animate');
             });	
