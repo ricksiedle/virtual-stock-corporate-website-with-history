7b0a77680bab74df2e2b73e6566bd8867ac41742
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-07-13 12:55:48 +0200

    Merge all changes

--- wp-content/themes/virtualstock/loop-templates/content-page.php
+++ wp-content/themes/virtualstock/loop-templates/content-page.php
@@ -301,12 +301,7 @@ global $post;
 
 										while ( have_rows('section_with_partner_logos') ) : the_row(); 
 			?>
-<<<<<<< HEAD
-											<div class="container-fluid v-wrapper v-wrapper-ret-sup v-full-width parallax-window" data-parallax="scroll" data-image-src="<?php echo get_sub_field('p_bg_image'); ?>" >
-=======
-						
-											<div class="container-fluid v-wrapper v-partners v-wrapper-ret-sup v-full-width" style="background-image:url('<?php echo get_sub_field('p_bg_image'); ?>')">
->>>>>>> e485275278b6f277e8c075cab85aa867429c8bf8
+											<div class="container-fluid v-wrapper v-partners v-wrapper-ret-sup v-full-width parallax-window" data-parallax="scroll" data-image-src="<?php echo get_sub_field('p_bg_image'); ?>" >
 												<div class="v-wrapper-ret-sup-heading">
 													<h2 class="text-align-center"><?php echo get_sub_field('p_heading'); ?></h2>
 												</div>
