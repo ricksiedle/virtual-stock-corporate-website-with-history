33704f66374e58cf85b4cdf4400612abaca5b7b5
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-09-06 12:28:34 +0200

    Remove line breaks on script tag for GTM

--- wp-content/themes/virtualstock/header.php
+++ wp-content/themes/virtualstock/header.php
@@ -13,9 +13,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <html <?php language_attributes(); ?>>
 <head>
 	<!-- Google Tag Manager -->
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-5WDJHR2');</script>
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-5WDJHR2');</script>
 	<!-- End Google Tag Manager -->
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
