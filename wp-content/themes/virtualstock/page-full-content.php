674b323b681129cb2ecd480de36ce6a134084365
sanja_symphony <sanja.jovanovikj@symphony-solutions.eu>
2019-11-08 14:15:24 +0100

    full content landing page changes

--- wp-content/themes/virtualstock/page-full-content.php
+++ wp-content/themes/virtualstock/page-full-content.php
@@ -16,50 +16,52 @@ get_header();
     <div class="solutions-boxes full-content">               
         <div class="container">
             <h1><?php the_field('header'); ?></h1> 
-            <div class="row">
-                <div class="col"> 
-                    <a href="#first">
-                        <div class="box-wrapper full-content-boxes">
-                            <div class="inner-box-wrapper">
-                                <h2><?php the_field('left_box_title'); ?></h2>
-                                <p><?php the_field('left_box_paragraph'); ?></p>
+            <div class="full-content-boxes-wrapper">
+                <div class="row">
+                    <div class="col"> 
+                        <a href="#first">
+                            <div class="box-wrapper full-content-boxes">
+                                <div class="inner-box-wrapper">
+                                    <h2><?php the_field('left_box_title'); ?></h2>
+                                    <p><?php the_field('left_box_paragraph'); ?></p>
+                                </div>
                             </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#second">
+                            <div class="box-wrapper full-content-boxes">
+                                <div class="inner-box-wrapper">  
+                                    <h2><?php the_field('right_box_title'); ?></h2>
+                                    <p><?php the_field('right_box_paragraph'); ?></p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>        
                 </div>
-                <div class="col">
-                    <a href="#second">
-                        <div class="box-wrapper full-content-boxes">
-                            <div class="inner-box-wrapper">  
-                                <h2><?php the_field('right_box_title'); ?></h2>
-                                <p><?php the_field('right_box_paragraph'); ?></p>
-                            </div>
-                        </div>
-                    </a>
-                </div>        
-            </div>
             
-            <div class="row">
-                <div class="col"> 
-                    <a href="#third">
-                        <div class="box-wrapper full-content-boxes">
-                            <div class="inner-box-wrapper">
-                                <h2><?php the_field('second_row_left_box_title'); ?></h2>
-                                <p><?php the_field('second_row_left_box_paragraph'); ?></p>
+                <div class="row">
+                    <div class="col"> 
+                        <a href="#third">
+                            <div class="box-wrapper full-content-boxes">
+                                <div class="inner-box-wrapper">
+                                    <h2><?php the_field('second_row_left_box_title'); ?></h2>
+                                    <p><?php the_field('second_row_left_box_paragraph'); ?></p>
+                                </div>
                             </div>
-                        </div>
-                    </a>
+                        </a>
+                    </div>
+                    <div class="col">
+                        <a href="#fourth">
+                            <div class="box-wrapper full-content-boxes">
+                                <div class="inner-box-wrapper">  
+                                    <h2><?php the_field('second_row_right_box_title'); ?></h2>
+                                    <p><?php the_field('second_row_right_box_paragraph'); ?></p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>        
                 </div>
-                <div class="col">
-                    <a href="#fourth">
-                        <div class="box-wrapper full-content-boxes">
-                            <div class="inner-box-wrapper">  
-                                <h2><?php the_field('second_row_right_box_title'); ?></h2>
-                                <p><?php the_field('second_row_right_box_paragraph'); ?></p>
-                            </div>
-                        </div>
-                    </a>
-                </div>        
             </div>
         </div>
     </div>  
